0f0cbd0e966912a0ee6c0e7315d097ed92a8ae96
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-08-13 00:30:44 -0400

    latitude

--- src/index.js
+++ src/index.js
@@ -7,23 +7,37 @@ class App extends React.Component {
 
     //THE ONLY TIME YOU DO DIRECT ASSIGMENT TO SET STATE
     //IS WHEN YOU FIRST CALL IT IN THE CONSTRUCTOR APP
-    this.state = { lat: null };
+    this.state = {
+      lat: null,
+      errorMessage: " "
+    };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
-        console.log(position);
+        // console.log(position);
 
         //SET STATE FUNCTION IS TO UPDATE STATE
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        // console.log(err);
+        this.setState({
+          errorMessage: "Unable to render location given that, " + err.message
+        });
+      }
     );
 
     /***lat=lattitude */
   }
 
   render() {
-    return <div>Latitude:{this.state.lat} </div>;
+    return (
+      <div>
+        Latitude:{this.state.lat}
+        <br />
+        Error:{this.state.errorMessage}{" "}
+      </div>
+    );
   }
 }
 
